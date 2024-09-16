79932221b9f4db642f33a5dc71ae5b312dda8377
SushilNagdev <156531818+SushilNagdev@users.noreply.github.com>
2024-09-16 19:12:52 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./components/Hero"
+import Navigation from "./components/Navigation"
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection/>
+      
+    </div>
+  )
+  
+}
+export default App
